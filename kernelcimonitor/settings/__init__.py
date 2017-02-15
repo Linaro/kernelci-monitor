2e23fbef1131431da88aef4aa9b87a2630c45e39
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2017-02-15 19:04:47 +0000

    Fixed path to the database file
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- kernelcimonitor/settings/__init__.py
+++ kernelcimonitor/settings/__init__.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'kernelcimonitor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.environ.get('DB_PATH', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 
