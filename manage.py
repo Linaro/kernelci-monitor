d56a026f811aa5e8f966e2a9ddadee0c0f90ac73
Antonio Terceiro <terceiro@softwarelivre.org>
2017-03-06 14:54:24 +0100

    manage.py: support local settings

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kernelcimonitor.settings")
+    if os.path.exists('kernelcimonitor/settings/local.py'):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kernelcimonitor.settings.local")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kernelcimonitor.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
