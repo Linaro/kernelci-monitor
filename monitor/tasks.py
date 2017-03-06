c5d7da8421cf9a5cd1b40f6c40132999be59dc67
Antonio Terceiro <terceiro@softwarelivre.org>
2017-03-06 15:26:20 +0100

    tasks: remove redundant logging configuration
    Let's let the logging be setup by Django

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -52,13 +52,6 @@ except ImportError:
     import httplib as http_client
 http_client.HTTPConnection.debuglevel = 1
 
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
-
 
 class LAVADefinitionException(Exception):
     pass
