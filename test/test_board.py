2f8dc1fe7918b01704810b09db6e5f153ba84656
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2017-03-14 21:17:45 +0000

    Added unit tests
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- test/test_board.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from monitor.models import Board
-
-class BoardTest(TestCase):
-
-    def test_defconfig(self):
-        board = Board(defconfiglist='foo bar')
-        self.assertEqual(['foo', 'bar'], board.defconfigs)
